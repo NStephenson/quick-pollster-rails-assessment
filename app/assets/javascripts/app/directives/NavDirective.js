c57564e11b8a55881d2b7744d7e6c77c1a7ff749
NStephenson <nickstephenson90@gmail.com>
2016-07-02 20:39:12 -0400

    added navbar directive

--- app/assets/javascripts/app/directives/NavDirective.js
+++ app/assets/javascripts/app/directives/NavDirective.js
@@ -1,1 +1,14 @@
-app.directive('qpNav')
+app.directive('qpNav', function(){
+  return {
+    restrict: 'E',
+    templateUrl: 'app/templates/nav/navbar.html',
+    controller: function(Auth){
+      var ctrl = this;
+
+      Auth.currentUser().then(function(user) { 
+        ctrl.currentUser = user; 
+      });
+    },
+    controllerAs: 'vm'
+  }
+});
