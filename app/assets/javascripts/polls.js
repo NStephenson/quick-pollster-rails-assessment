6a1fee35197e84bab9f8912ffcaa0f5fbbd27c51
NStephenson <nickstephenson90@gmail.com>
2016-07-20 13:49:17 -0400

    added poll constructor

--- app/assets/javascripts/polls.js
+++ app/assets/javascripts/polls.js
@@ -5,3 +5,18 @@ getCurrentUser();
 var authToken = $('meta[name="csrf-token"]').attr('content');
 var currentUser;
 var allPolls = [];
+
+class Poll {
+  constructor(id, question, published, selectMultiple, publicResults, open, responses, user) {
+    this.id = id;
+    this.question = question;
+    this.published = published;
+    this.selectMultiple = selectMultiple;
+    this.publicResults = publicResults;
+    this.open = open;
+    this.responses = responses;
+    this.user = user;
+  }
+
+  }
+}
