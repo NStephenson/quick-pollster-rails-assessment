5496ae277f73b62d432c04bce6930aac7fd0374e
NStephenson <nickstephenson90@gmail.com>
2016-08-05 03:39:38 -0400

    added simple validators

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,10 @@
 class Category < ActiveRecord::Base
 
   belongs_to :poll
+
+  validates :poll, presence: true
+  validates :name, presence: true
+  validates :name, format: { with: /\A[a-zA-Z]+\z/,
+    message: "only allows letters" }
+
 end
