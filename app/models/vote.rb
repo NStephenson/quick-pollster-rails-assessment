86d1dad2fd2fc3f25397b882c8839d3b98fd10f6
NStephenson <nickstephenson90@gmail.com>
2016-08-05 02:51:55 -0400

    added validations

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -4,6 +4,12 @@ class Vote < ActiveRecord::Base
   belongs_to :response
   has_one :poll, through: :response
 
+  validates :user, presence: true
+  validates :response, presence: true
+  validates :response, uniqueness: {scope: :user, 
+    message: 'You\'ve already made this vote!' 
+  }
+
   def self.public_votes
     where(public_response: true)
   end
