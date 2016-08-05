d44d488eca9402bc8f99039b956c79e0b61ff96e
NStephenson <nickstephenson90@gmail.com>
2016-08-05 03:31:45 -0400

    added categories model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160524213442) do
+ActiveRecord::Schema.define(version: 20160805072140) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "poll_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "poll_surveys", force: :cascade do |t|
     t.integer  "poll_id"
